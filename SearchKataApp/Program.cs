819c5ee098c9ef5e5e0fda0732bd4161ca18365d
yridesconix <yridesconix@yahoo.com>
2019-02-24 23:26:27 -0500

    Code complete

--- SearchKataApp/Program.cs
+++ SearchKataApp/Program.cs
@@ -10,6 +10,25 @@ namespace SearchKataApp
     {
         static void Main(string[] args)
         {
+            var loader = new Loader();
+
+            Console.WriteLine("Enter filepath:");
+            var path = Console.ReadLine();
+            loader.LoadFile(path);
+
+            Console.WriteLine("File loaded. Ready?");
+            Console.ReadKey();
+
+            var search = new Search(loader.Data, loader.SearchTerms);
+            var matches = search.ExecuteSearch();
+
+            foreach (var match in matches.OrderBy(x => x))
+            {
+                Console.WriteLine(match);
+            }
+
+            Console.WriteLine("Done?");
+            Console.ReadKey();
         }
     }
 }
--- SearchKataApp/README.md
+++ SearchKataApp/README.md
@@ -11,6 +11,16 @@ Once installed, execute the following
 msbuild "C:\git\SearchKataApp\SearchKataApp\searchKataApp.sln" /t:Rebuild /p:Configuration=Release /p:Platform="any cpu"
 ```
 
+### Install mstest 
+### navigate to msbuild.exe 
+
+installation may be in C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE
+Once installed, execute the following 
+
+```cmd.exe 
+mstest /testcontainer:SearchKataUnitTests.dll
+```
+
 ## Run
 From the solution folder 
 
